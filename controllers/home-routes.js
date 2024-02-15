196672ffa37f85d3f35ca2cccd2e0977b8f7bdf5
Eric Peterson <ericmpeterson04@gmail.com>
2024-02-15 12:28:35 -0700

    Got posts to render on a separate view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
@@ -32,31 +32,60 @@ router.get('/', async (req, res) => {
 //GET one post
 router.get('/post/:id', withAuth, async (req, res) => {
   try {
-    const dbPostData = await Post.findByPk(req.params.id);
+    // Fetch the post by ID from the database
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment, 
+          attributes: ['body'],
+        },
+      ],
+    })
 
+    if (!dbPostData) {
+      return res.status(404).json('Post not found');
+    }
+    
     const post = dbPostData.get({ plain: true });
-    console.log(post);
-    res.render('post', { post, loggedIn: req.session.loggedIn });
+    
+    res.render('post', { 
+      post, 
+      loggedIn: req.session.loggedIn,
+    });
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
-  }
-});
+    }
+  });
 
-//GET all comments
-router.get('/comment/:id', withAuth, async (req, res) => {
-  try {
-    const dbCommentData = await Comment.findByPk(req.params.id);
+  /*
+  router.get('/post/:id', async (req, res) => {
+    try {
+      const dbCommentData = await Comment.findAll({
+        include: [
+          {
+            model: User, 
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      const comments = dbCommentData.map((comment) =>
+        comment.get({ plain: true })
+      );
+      console.log(comments);
+      res.render('comments', {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
-    const comment = dbCommentData.get({ plain: true });
-    console.log(comment);
-    res.render('comment', { comment, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
+  */
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
