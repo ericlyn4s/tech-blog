e17737ce0e0c588d2657ae1bb3c0f0fa0e448923
Eric Peterson <ericmpeterson04@gmail.com>
2024-02-16 15:44:56 -0700

    Misc. changes

--- models/index.js
+++ models/index.js
@@ -2,30 +2,26 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-
-User.hasMany(Post, {
-  foreignKey: 'user_id',
-});
-
 Post.belongsTo(User, {
   foreignKey: 'user_id',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'use_id',
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
+// since we are going to be retrieving the post and then its associated comments, we would use Post.hasMany
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
+  onDelete: 'CASCADE',
 });
-
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-});
-
 
 module.exports = { User, Post, Comment };
+
+/*
+What view the user hits determines what controller they hit
+What controller is hit determines what model is hit and for what data
+What model is hit determines what data comes back
+*/
--- public/js/createpost.js
+++ public/js/createpost.js
@@ -3,7 +3,7 @@ const newPostHandler =  async (event) => {
   
     const title = document.querySelector('#post-title').value;
     const content = document.querySelector('#post-content').value;
-    const user_id = parseInt(2);
+    const user_id = parseInt(1);
  
     if (title && content) {
       const response = await fetch('/api/createposts', {
